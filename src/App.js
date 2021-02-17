6746fc52c2af8fc904445fc41caaa6b6b6b6bfe7
Wagner S. Vieira <w.silveira.vieira@gmail.com>
2021-02-16 22:59:41 -0400

    informacoes dos hosts carregadas no popup do respectivo marcador

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   state = {
     auth: "",
     hosts: [],
-    hostsCopy: [],
+    problems: [],
   };
 
   zabbix = new Z("177.53.204.46/zabbix/api_jsonrpc.php");
@@ -31,11 +31,55 @@ class App extends Component {
       jsonrpc: 2.0,
       method: "host.get",
       params: {
-        output: ["host", "status"],
+        output: ["host", "name", "status"],
         selectInventory: ["location_lat", "location_lon"],
+        selectInterfaces: ["interfaceid", "ip"],
+        selectTriggers: [],
       },
       auth: this.state.auth,
       id: 2,
+    });
+  };
+
+  getItems = () => {
+    return this.zabbix.call({
+      jsonrpc: 2.0,
+      method: "item.get",
+      params: {
+        filter: { key_: ["icmpping", "icmppingloss", "icmppingsec"] },
+        output: [
+          "key_",
+          "hostid",
+          "name",
+          "lastclock",
+          "lastns",
+          "lastvalue",
+          "prevvalue",
+        ],
+        monitored: true,
+      },
+      auth: this.state.auth,
+      id: 3,
+    });
+  };
+
+  getProblems = () => {
+    return this.zabbix.call({
+      jsonrpc: 2.0,
+      method: "problem.get",
+      params: {
+        output: [
+          "eventid",
+          "objectid",
+          "clock",
+          "ns",
+          "name",
+          "severity",
+          "opdata",
+        ],
+      },
+      auth: this.state.auth,
+      id: 4,
     });
   };
 
@@ -44,11 +88,61 @@ class App extends Component {
     this.setState({ auth: auth });
 
     const hosts = await this.gethosts();
-    this.setState({ hosts: hosts, hostsCopy: hosts });
+    const items = await this.getItems();
+    const problems = await this.getProblems();
+
+    hosts.forEach((host) => {
+      host.items = [];
+      host.problems = [];
+      items.forEach((item) => {
+        if (item.hostid === host.hostid) {
+          host.items.push(item);
+        }
+      });
+
+      problems.forEach((problem) => {
+        host.triggers.forEach((trigger) => {
+          if (trigger.triggerid === problem.objectid) {
+            host.problems.push(problem);
+          }
+        });
+      });
+    });
+
+    this.setState({ hosts: hosts, problems: problems });
+
+    this.start();
+  };
+
+  start = () => {
+    setInterval(async () => {
+      const hosts = await this.gethosts();
+      const items = await this.getItems();
+      const problems = await this.getProblems();
+
+      hosts.forEach((host) => {
+        host.items = [];
+        host.problems = [];
+        items.forEach((item) => {
+          if (item.hostid === host.hostid) {
+            host.items.push(item);
+          }
+        });
+
+        problems.forEach((problem) => {
+          host.triggers.forEach((trigger) => {
+            if (trigger.triggerid === problem.objectid) {
+              host.problems.push(problem);
+            }
+          });
+        });
+      });
+
+      this.setState({ hosts: hosts, problems: problems });
+    }, 10000);
   };
 
   render() {
-    console.log(this.state);
     return (
       <React.Fragment>
         <Map hosts={this.state.hosts} />
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,51 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup, Pane } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import icon from "../img/marker.png";
-import Search from "../components/Search";
 
-function Map(props) {
-  const myIcon = L.icon({
+class Map extends Component {
+  state = {
+    position: [-20.464615419359156, -54.61627937784175],
+    zoom: 12,
+    scrollWheelZoom: true,
+  };
+
+  myIcon = L.icon({
     iconUrl: icon,
     iconSize: [40, 40],
     iconAnchor: [20, 40],
     popupAnchor: [0, -40],
   });
-  const center = [51.505, -0.09];
-  const rectangle = [
-    [51.49, -0.08],
-    [51.5, -0.06],
-  ];
 
-  const position = [-20.464615419359156, -54.61627937784175];
+  render() {
+    return (
+      <MapContainer
+        id="map"
+        center={this.state.position}
+        zoom={this.state.zoom}
+        scrollWheelZoom={this.state.scrollWheelZoom}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-  return (
-    <MapContainer id="map" center={position} zoom={12} scrollWheelZoom={true}>
-      <Pane>
-        <Search />
-      </Pane>
+        {this.props.hosts.map((host) => {
+          return (
+            <Marker
+              key={host.hostid}
+              position={[
+                host.inventory.location_lat,
+                host.inventory.location_lon,
+              ]}
+              icon={this.myIcon}
+            >
+              <Popup>
+                <ul className="ul-popup">
+                  <li>{host.name}</li>
+                  <li>{host.interfaces[0].ip}</li>
+                  <li>Latitude: {host.inventory.location_lat}</li>
+                  <li>Longitude: {host.inventory.location_lon}</li>
+                  {host.items.map((item) => {
+                    return (
+                      <li key={item.itemid}>
+                        {item.name}: {item.lastvalue}
+                      </li>
+                    );
+                  })}
 
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-
-      {props.hosts.map((host) => {
-        return (
-          <Marker
-            key={host.hostid}
-            position={[
-              host.inventory.location_lat,
-              host.inventory.location_lon,
-            ]}
-            icon={myIcon}
-          >
-            <Popup>{host.host}</Popup>
-          </Marker>
-        );
-      })}
-    </MapContainer>
-  );
+                  {host.problems.map((problem) => {
+                    return <li key={problem.eventid}>{problem.name}</li>;
+                  })}
+                </ul>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    );
+  }
 }
 
 export default Map;
